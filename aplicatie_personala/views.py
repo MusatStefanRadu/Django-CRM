ac66469d6058b2fc23e5c84c4274b23e2ad07827
MusatStefanRadu <musatstefan2004@gmail.com>
2024-10-22 02:20:57 +0300

    Partea 2

--- aplicatie_personala/views.py
+++ aplicatie_personala/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm 
 
 # Create your views here.
 from django.http import HttpResponse
@@ -34,5 +34,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-            return render(request, 'register.html', {})
+            if request.method == 'POST':
+                 form = SignUpForm(request.POST)
+                 if form.is_valid():
+                      form.save()
+                      #authenticate and login
+                      username = form.cleaned_data['username']
+                      password = form.cleaned_data['password1']
+
+                      user = authenticate(username=username, password=password)
+                      login(request, user)
+                      messages.success(request, "You have Succesfully Registered!")
+                      return redirect('home')
+            else:
+                form = SignUpForm()
+            return render(request, 'register.html', {'form':form})
 
